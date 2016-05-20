035fbc5bb8c9be9a432b67cc21172704eab6f105
Tautvydas Petkus <petkus09@gmail.com>
2016-05-21 00:07:10 +0300

    Progress. jacoco

--- src/edu/ktu/petkus/Coverage.java
+++ src/edu/ktu/petkus/Coverage.java
@@ -1,45 +1,93 @@
 package edu.ktu.petkus;
 
-import edu.ktu.test.petkus.CoverageTargets;
+import org.jacoco.core.analysis.Analyzer;
+import org.jacoco.core.analysis.CoverageBuilder;
+import org.jacoco.core.analysis.IClassCoverage;
+import org.jacoco.core.analysis.ICounter;
+import org.jacoco.core.data.ExecutionDataStore;
+import org.jacoco.core.data.SessionInfoStore;
+import org.jacoco.core.instr.Instrumenter;
+import org.jacoco.core.runtime.IRuntime;
+import org.jacoco.core.runtime.LoggerRuntime;
+import org.jacoco.core.runtime.RuntimeData;
 
-import java.lang.invoke.MethodHandle;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
+import java.io.PrintStream;
 
-/**
- * Created by ryselis on 16.5.14.
- */
 public class Coverage {
-    StackTraceElement[] initialTrace;
-    Class monitoredClass;
-
-    public void BeginCoverage(Class c){
-        initialTrace = Thread.currentThread().getStackTrace();
-        monitoredClass = c;
+    private final PrintStream out;
+    private String targetName = "";
+    private RuntimeData data;
+    private IRuntime runtime;
+    public Coverage(final PrintStream out) {
+        this.out = out;
     }
 
-    public void EndCoverage(Class c){
-        List<StackTraceElement> classStackTrace = TrimInitialFromTrace();
-//        for(int i = 0; i < trace.length; i++){
-//            if (trace[i].getClassName() == monitoredClass.getName()){
-//                classStackTrace.add(trace[i]);
-//            }
-//        }
+    public void BeginCoverage(Object instance) throws Exception {
+        targetName = instance.getClass().getName();
+
+        // For instrumentation and runtime we need a IRuntime instance
+        // to collect execution data:
+        runtime = new LoggerRuntime();
+
+        // The Instrumenter creates a modified version of our test target class
+        // that contains additional probes for execution data recording:
+        final Instrumenter instr = new Instrumenter(runtime);
+        final byte[] instrumented = instr.instrument(getTargetClass(targetName), targetName);
+
+        // Now we're ready to run our instrumented class and need to startup the
+        // runtime first:
+        data = new RuntimeData();
+        runtime.startup(data);
+
+        // In this tutorial we use a special class loader to directly load the
+        // instrumented class definition from a byte[] instances.
+        final MemoryClassLoader memoryClassLoader = new MemoryClassLoader();
+        memoryClassLoader.addDefinition(targetName, instrumented);
+        final Class<?> targetClass = memoryClassLoader.loadClass(targetName);
+
+        // Here we execute our test target class through its Runnable interface:
+        final Runnable targetInstance = (Runnable) targetClass.newInstance();
     }
 
-    public List<StackTraceElement> TrimInitialFromTrace(){
-        List<StackTraceElement> res = new ArrayList<StackTraceElement>();
-        StackTraceElement[] currentTrace = Thread.currentThread().getStackTrace();
-        for (int j=0;j<currentTrace.length;j++){
-            boolean duplicate = false;
-            for (int k=j+1;k<initialTrace.length;k++)
-                if (k!=j && initialTrace[k] == currentTrace[j])
-                    duplicate = true;
-            if (!duplicate)
-                res.add(currentTrace[j]);
+    public void EndCoverage() throws Exception{
+        // At the end of test execution we collect execution data and shutdown
+        // the runtime:
+        final ExecutionDataStore executionData = new ExecutionDataStore();
+        final SessionInfoStore sessionInfos = new SessionInfoStore();
+        data.collect(executionData, sessionInfos, false);
+        runtime.shutdown();
+
+        // Together with the original class definition we can calculate coverage
+        // information:
+        final CoverageBuilder coverageBuilder = new CoverageBuilder();
+        final Analyzer analyzer = new Analyzer(executionData, coverageBuilder);
+        analyzer.analyzeClass(getTargetClass(targetName), targetName);
+
+        // Let's dump some metrics and line coverage information:
+        for (final IClassCoverage cc : coverageBuilder.getClasses()) {
+            out.printf("Coverage of class %s%n", cc.getName());
+
+            printCounter("instructions", cc.getInstructionCounter());
+            printCounter("branches", cc.getBranchCounter());
+            printCounter("lines", cc.getLineCounter());
+            printCounter("methods", cc.getMethodCounter());
+            printCounter("complexity", cc.getComplexityCounter());
+
+            for (int i = cc.getFirstLine(); i <= cc.getLastLine(); i++) {
+                out.printf("Line %s: %s%n", Integer.valueOf(i));
+            }
         }
-        return res;
     }
 
+    public InputStream getTargetClass(final String name) {
+        final String resource = '/' + name.replace('.', '/') + ".class";
+        return getClass().getResourceAsStream(resource);
+    }
 
+    private void printCounter(final String unit, final ICounter counter) {
+        final Integer missed = Integer.valueOf(counter.getMissedCount());
+        final Integer total = Integer.valueOf(counter.getTotalCount());
+        out.printf("%s of %s %s missed%n", missed, total, unit);
+    }
 }
